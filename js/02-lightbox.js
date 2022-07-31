6e54e216750c82c3473b303f17cac8e3a64acbf6
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-01 01:03:54 +0300

    2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const template = ({ description, original, preview }) => `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+const galleryTemplate = galleryItems.map(galleryItem => template(galleryItem)).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryTemplate);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
